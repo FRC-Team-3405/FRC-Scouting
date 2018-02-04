14ccd81ec5eaabbcf96646ec296936f1dcde9945
Matthew Whitaker <sub6resources@gmail.com>
2018-02-03 17:32:21 -0700

    WIP: Continued

--- app/src/main/java/com/sub6resources/frcscouting/login/LoginRepository.kt
+++ app/src/main/java/com/sub6resources/frcscouting/login/LoginRepository.kt
@@ -1,24 +1,26 @@
 package com.sub6resources.frcscouting.login
 
 import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MediatorLiveData
-import android.arch.lifecycle.MutableLiveData
 import com.sub6resources.frcscouting.login.model.User
 import com.sub6resources.frcscouting.login.model.UserDao
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import java.util.*
+import io.grpc.ManagedChannelBuilder
 
 /**
  * Created by whitaker on 1/8/18.
  */
 class LoginRepository(val loginApi: LoginApi, val userDao: UserDao) {
 
-    /*fun signIn(login: Login): LiveData<BasicNetworkState<User>> = makeNetworkRequest(loginApi.signIn(login), userDao.signIn(login.username)) {
-        insert { userDao.create(it.apply { username = login.username }) }
-    }*/
-
     fun signIn(login: Login): LiveData<BasicNetworkState<User>> = makeNetworkRequest(loginApi.signIn(login), userDao.signIn(login.username)) {
         insert { userDao.create(it.apply { username = login.username })}
+    }
+
+
+    val channel by lazy {
+        ManagedChannelBuilder.forAddress("10.0.2.2", 8080)
+                .usePlaintext(true)
+                .build()
+    }
+    val stub by lazy {
+
     }
 }
